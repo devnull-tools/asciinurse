7877abf6935bc51312750c6a6f43e0231332d9a3
Ataxexe <ataxexe@devnull.tools>
2015-07-14 20:44:38 -0300

    add support for custom scripts

--- lib/asciinurse.rb
+++ lib/asciinurse.rb
@@ -2,3 +2,9 @@ require 'asciidoctor'
 
 require_relative 'asciinurse/version'
 require_relative 'asciinurse/extensions/charts/chart_extension'
+
+ENV['ASCIINURSE_USER_DIR'] ||= ('%s/.asciinurse' % ENV['HOME'])
+if File.exist? ENV['ASCIINURSE_USER_DIR']
+  custom_script = '%s/asciinurse.rb' % ENV['ASCIINURSE_USER_DIR']
+  require custom_script if File.exist? custom_script
+end

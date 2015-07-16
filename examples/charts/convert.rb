ec660fee40d8b3a7919827fbc2be01e5900ead1e
Ataxexe <ataxexe@devnull.tools>
2015-07-15 23:06:00 -0300

    update examples

--- examples/charts/convert.rb
+++ examples/charts/convert.rb
@@ -1,6 +1,6 @@
 require 'asciidoctor'
 require 'asciidoctor-pdf'
-require_relative '../lib/asciinurse'
+require_relative '../../lib/asciinurse'
 
 Asciidoctor.convert_file ARGV[0], header_footer: true, safe: :unsafe
 Asciidoctor.convert_file ARGV[0], backend: 'pdf', header_footer: true, safe: :unsafe

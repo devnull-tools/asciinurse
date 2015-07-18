aa8fdaec0190235d2e70b1a369e70ee35edc6706
Ataxexe <ataxexe@devnull.tools>
2015-07-17 21:35:30 -0300

    export images for google charts

---
+++ resources/googlecharts/converter/googlecharts-convert.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var page = require('webpage').create();
+var system = require('system');
+
+var args = system.args;
+
+var scriptDir = args[0].replace(/googlecharts-convert\.js$/,'')
+var config = fs.read(args[1])
+var pageContent = fs.read(scriptDir + 'template.html')
+pageContent = pageContent.replace(/\$\{CONFIG}/g, config);
+
+var path = args[1] + '.html';
+fs.write(path, pageContent, 'w');
+
+page.viewportSize = {
+    width: 1024,
+    height: 768
+};
+
+page.open(args[1] + '.html', function() {
+    window.setTimeout(function () {
+        page.render(args[2]);
+        phantom.exit();
+    }, 1000);
+});

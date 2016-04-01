6a8a517a68407083ab66e4f64edc3203a76c15b9
Ataxexe <ataxexe@devnull.tools>
2016-04-01 17:57:32 -0300

    fix google chart engine

--- resources/googlecharts/javascripts/global.js
+++ resources/googlecharts/javascripts/global.js
@@ -1,8 +1,8 @@
-google.load("visualization", "1.1", {packages:["corechart", "bar", "line"]});
-var drawChart = function(id, config) {
+google.charts.load("current", {packages: ["corechart", "bar", "line"]});
+var drawChart = function (id, config) {
     var div = document.getElementById(id)
     div.style.width = config['style']['width']
     div.style.height = config['style']['height']
-    var chart = new config['type'](div);
+    var chart = eval("new " + config['type'] + "(div)");
     chart.draw(config['data'], config['options']);
 }
